336171f208610f5a029bf75d745269d699380cbc
chaim <chaimyisroel@gmail.com>
2021-10-04 21:28:56 -0400

    commit tree 57a70735d1aa01c737da7be18228ad59faf70514

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/' do 
+    'Hello world'
+  end
+
+  get '/messages' do
+    messages = Message.order('created_at')
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(params)
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(body: params[:body], username: params[:username])
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+  end
 end
